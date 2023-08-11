a5857d0d1d07077a58b5de6d0f05caab255e7ad9
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-08-11 15:44:25 +0100

    Patch/provider id to integer (#3)
    * amend provider id to be an int not string
    
    * push for a build

--- caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/repository/ApplicationRepositoryIntegrationTest.java
+++ caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/repository/ApplicationRepositoryIntegrationTest.java
@@ -4,7 +4,6 @@ package uk.gov.laa.ccms.data.api.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.annotation.CacheAnnotationParser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 import uk.gov.laa.ccms.api.CAABApplication;
