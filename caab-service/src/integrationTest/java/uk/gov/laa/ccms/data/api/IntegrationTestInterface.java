6f38506a2b9636c81640f2d320a620a1108c77d5
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-06-05 16:53:18 +0100

    use auth starter

--- caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/IntegrationTestInterface.java
+++ caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/IntegrationTestInterface.java
@@ -19,6 +19,10 @@ public interface IntegrationTestInterface {
     registry.add("spring.datasource.url", oracleContainerSingleton.getOracleContainer()::getJdbcUrl);
     registry.add("spring.datasource.username", oracleContainerSingleton.getOracleContainer()::getUsername);
     registry.add("spring.datasource.password", oracleContainerSingleton.getOracleContainer()::getPassword);
+
+    registry.add("laa.ccms.springboot.starter.auth.authorized-clients", () -> "[{\"name\":\"caab-ui\",\"roles\":[\"ALL\"],\"token\":\"c196393a-f279-45ba-b5d5-f93e6d30465a\"}]");
+    registry.add("laa.ccms.springboot.starter.auth.authorized-roles", () -> "[{\"name\":\"ALL\",\"URIs\":[\"/**\"]}]");
+    registry.add("laa.ccms.springboot.starter.auth.unprotected-uris", () -> "[\"\"]");
   }
 }
 
