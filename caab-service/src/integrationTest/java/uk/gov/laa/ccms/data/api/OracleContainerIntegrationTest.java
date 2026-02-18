24bf57858c6454d8dd25f7dfba4b5713608f2a96
farrell-m <michael.farrell@justice.gov.uk>
2026-02-18 16:52:04 +0000

    restructure integration tests

--- caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/OracleContainerIntegrationTest.java
+++ caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/OracleContainerIntegrationTest.java
@@ -1,24 +1,19 @@
 package uk.gov.laa.ccms.data.api;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.oracle.OracleContainer;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @DirtiesContext
-public class OracleContainerIntegrationTest {
+public abstract class OracleContainerIntegrationTest {
 
     @Container
     @ServiceConnection
     public static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-free:23-slim-faststart")
             .withUsername("XXCCMS_PUI")
-            .withPassword("XXCCMS_PUI")
-            .withReuse(true);
-
+            .withPassword("XXCCMS_PUI");
 }
 
--- caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/controller/LinkedCaseControllerIntegrationTest.java
+++ caab-service/src/integrationTest/java/uk/gov/laa/ccms/data/api/controller/LinkedCaseControllerIntegrationTest.java
@@ -1,21 +1,54 @@
 package uk.gov.laa.ccms.data.api.controller;
 
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-import static org.springframework.test.context.jdbc.SqlMergeMode.MergeMode.MERGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlMergeMode;
-import uk.gov.laa.ccms.caab.api.CaabApiApplication;
+import uk.gov.laa.ccms.caab.api.controller.LinkedCaseController;
+import uk.gov.laa.ccms.caab.api.service.ApplicationService;
+import uk.gov.laa.ccms.caab.model.LinkedCaseDetail;
 
-@SpringBootTest(classes = CaabApiApplication.class)
-@SqlMergeMode(MERGE)
-@Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "/sql/application_tables_create_schema.sql")
-@Sql(executionPhase = AFTER_TEST_METHOD, scripts = "/sql/application_tables_drop_schema.sql")
-public class LinkedCaseControllerIntegrationTest extends BaseLinkedCaseControllerIntegrationTest {
+public class LinkedCaseControllerIntegrationTest
+    extends AbstractControllerIntegrationTest {
 
-    //this runs all tests in BaseLinkedCaseControllerIntegrationTest, do not add anything here
-    //running this class takes longer than the local version, but it is used for running tests in a pipeline
+  @Autowired
+  private LinkedCaseController linkedCaseController;
 
+  @Autowired
+  private ApplicationService applicationService;
+
+  @Test
+  @Sql(scripts = {"/sql/application_insert.sql", "/sql/linked_cases_insert.sql"})
+  public void updateLinkedCase() throws Exception {
+    Long caseRef = 41L;
+    Long linkedCaseRef = 2L;
+
+    LinkedCaseDetail linkedCase = loadObjectFromJson("/json/linked_cases_new.json", LinkedCaseDetail.class);
+    linkedCase.setId(linkedCaseRef.intValue());
+
+    linkedCaseController.updateLinkedCase(linkedCaseRef, caabUserLoginId, linkedCase);
+
+    List<LinkedCaseDetail> linkedCases = applicationService.getLinkedCasesForApplication(caseRef);
+
+    assertEquals(1, linkedCases.size());
+
+    // Remove the audittrail for comparison purposes.
+    LinkedCaseDetail retrievedLinkedCase = linkedCases.get(0);
+    retrievedLinkedCase.setAuditTrail(null);
+
+    assertEquals(linkedCase, retrievedLinkedCase);
+  }
+
+  @Test
+  @Sql(scripts = {"/sql/application_insert.sql", "/sql/linked_cases_insert.sql"})
+  public void deleteLinkedCase() {
+    Long caseRef = 41L;
+    Long linkedCaseRef = 2L;
+
+    linkedCaseController.removeLinkedCase(linkedCaseRef, caabUserLoginId);
+    List<LinkedCaseDetail> linkedCases = applicationService.getLinkedCasesForApplication(caseRef);
+    assertEquals(0, linkedCases.size());
+  }
 }
